9a780ecdc4146aeaf28999ccc859ca6cf3bba120
Samessprog <100785560+Samessprog@users.noreply.github.com>
2023-05-17 13:42:04 +0200

    Better pagination hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter APP',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -46,10 +46,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> getData() async {
-    final url = Uri.parse('https://fluttermysqlsames.000webhostapp.com/getData.php');
+    final url =  Uri.parse('https://fluttermysqlsames.000webhostapp.com/getData.php');
     final response = await http.get(url);
     setState(() {
       _data = jsonDecode(response.body);
+
     });
   }
 
@@ -93,6 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     }
   }
+
   int _currentPage = 0;
   final int _perPage = 10;
   double _value = 5.0;
@@ -109,8 +111,10 @@ class _MyHomePageState extends State<MyHomePage> {
       _endIndex = (_startIndex + _perPage < _data.length)
           ? _startIndex + _perPage
           : _data.length;
-    }
 
+      print(_startIndex);
+      print(_endIndex);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -179,17 +183,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   cells: [
                     DataCell(
                       Text(
-                        _data[index]['name'],
+                        _data[_startIndex + index]['name'],
                         style: TextStyle(
                           fontSize: 17,
-
                         ),
                       ),
                     ),
 
                     DataCell(
                       Text(
-                        _data[index]['Miasto'],
+                        _data[_startIndex + index]['Miasto'],
                         style: TextStyle(fontSize: 17),
                       ),
                     ),
@@ -201,7 +204,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             builder: (BuildContext context) {
                               return AlertDialog(
                                 title: const Text('Opis'),
-                                content: Text(_data[index]['opis']),
+                                content: Text(_data[_startIndex + index]['opis']),
                                 actions: [
                                   TextButton(
                                     onPressed: () {
@@ -220,7 +223,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     DataCell(
                       ElevatedButton(
                         onPressed: () {
-                          _showImage(context, _data[index]['img']);
+                          _showImage(context, _data[_startIndex + index]['img']);
                         },
                         child: Text('Show photo'),
                         style: ElevatedButton.styleFrom(
@@ -232,7 +235,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       Row(
                         children: [
                           Text(
-                            _data[index]['ocena'],
+                            _data[_startIndex + index]['ocena'],
                             style: TextStyle(fontSize: 17),
                           ),
                           Icon(Icons.star, color: Colors.yellow),
@@ -242,7 +245,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     DataCell(
                       ElevatedButton(
                         onPressed: () {
-                          Clipboard.setData(ClipboardData(text: _data[index]['Googlge_link']));
+                          Clipboard.setData(ClipboardData(text: _data[_startIndex + index]['Googlge_link']));
                         },
                         child: Text('Link'),
                       ),
@@ -272,13 +275,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
-
-                // Current page indicator
+                // Aktualna strona
                 Text('Page ${_currentPage + 1} of $_totalPages'),
-                // Next page button
                 Container(
                   margin: EdgeInsets.only(left: 10),
-
                   child: ElevatedButton(
                     onPressed: (_currentPage == _totalPages - 1)
                         ? null
@@ -299,12 +299,7 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(height: 25),
             SliderTheme(
               data: SliderThemeData(
-                thumbColor: Colors.blue,
-                activeTrackColor: Colors.blue,
-                inactiveTrackColor: Colors.grey,
-                overlayColor: Colors.blue.withAlpha(32),
-                valueIndicatorColor: Colors.green,
-                trackHeight: 8.0, // wysokość ścieżki
+                // Konfiguracja wyglądu suwaka
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -317,23 +312,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   SizedBox(
-                width: 350,
-                child: Slider(
-                  value: _value,
-                  min: 1.0,
-                  max: 10.0,
-                  divisions: 9,
-                  label: '$_value km',
-                  onChanged: (value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
-                ),
-              ),
+                    width: 350,
+                    child: Slider(
+                      value: _value,
+                      min: 1.0,
+                      max: 10.0,
+                      divisions: 9,
+                      label: '$_value km',
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      },
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
